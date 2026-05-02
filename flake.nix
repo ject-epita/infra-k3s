cd355350cdaa7750fba7b4d414391bc9ab754df3
Leo THIVILLON <kernelb00t@github.com>
2026-05-02 14:54:05 +0530

    feat: add kseal utility to development shell

--- flake.nix
+++ flake.nix
@@ -21,7 +21,33 @@
         shellHook = ''
           export KUBECONFIG="$PWD/kubeconfig"
         '';
-        packages = with pkgs; [
+        packages = with pkgs; let
+          kseal = python3Packages.buildPythonApplication rec {
+            pname = "kseal";
+            version = "2.0.2";
+            pyproject = true;
+            src = fetchFromGitHub {
+              owner = "eznix86";
+              repo = "kseal";
+              rev = "v${version}";
+              sha256 = "09v9vcy2qh6dczz8k0fbmj3hkb5p7p1b9wkilrvisv86wy1ydaaf";
+            };
+            nativeBuildInputs = with python3Packages; [
+              hatchling
+            ];
+            propagatedBuildInputs = with python3Packages; [
+              click
+              httpx
+              kubernetes
+              packaging
+              pydantic
+              rich
+              ruamel-yaml
+            ];
+            # Pas de tests dans le repo github pour cette version
+            doCheck = false;
+          };
+        in [
           (wrapHelm kubernetes-helm {
             plugins = with pkgs.kubernetes-helmPlugins; [
               helm-diff
@@ -42,6 +68,7 @@
           python3
           kubeseal
           argocd
+          kseal
         ];
       };
     });
